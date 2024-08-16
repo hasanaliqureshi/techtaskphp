284b1857fa69aab0b323300f7d86086acd5b92c2
hasanaliqureshi <hasanali.zeds@gmail.com>
2024-08-16 15:53:27 +0500

    php file added

---
+++ backend/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+  // 1. RUN THIS API 5 TIMES TO COLLECT USER DATA IN VARIABLE (https://randomuser.me/api)
+
+  // 2. SORT DATA TO RETURN JSON IN THIS FORMAT
+
+  // 3. [
+  //   {
+  //     "first": "",
+  //     "last": "",
+  //     "city": "",
+  //     "state": "",
+  //     "country": ""
+  //   } .....
+  // ]
+
+
+
+
+?>
