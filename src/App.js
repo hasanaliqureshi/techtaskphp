0c60ad370f8896e49d96d88d5d044ae01174caba
hasanaliqureshi <hasanali.zeds@gmail.com>
2024-08-16 12:25:25 +0500

    init 🚀

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    // 1. GET USERS FROM API (https://inspon-app.com/test.php)
+
+    // 2. USE COMPONENT TO RENDER DATA INSIDE USER COMPONENT
+
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default App;
